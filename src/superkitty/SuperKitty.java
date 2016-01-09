ce7771dad41316184e32e6b1fc7a750f13b86d25
Tobias Eydam <tobiaseydam@hotmail.com>
2016-01-09 21:56:30 +0100

    Initial Commit und erste Idee zum Spielfeld

---
+++ src/superkitty/SuperKitty.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package superkitty;
+
+/**
+ *
+ * @author Tobias
+ */
+public class SuperKitty {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static MainForm mf = new MainForm(20,30);
+    
+    public static void main(String[] args) {
+        mf.setVisible(true);
+    }
+    
+}
